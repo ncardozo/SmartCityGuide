aa37159003b2dee20b467a424ee9702231d0d9d2
Nicolas Cardozo <ncardozo@vub.ac.be>
2015-11-16 18:25:47 +0000

    started completing the missing implementation of the methods
    Signed-off-by: Nicolas Cardozo <ncardozo@vub.ac.be>

--- js/js/guidedVisit.js
+++ js/js/guidedVisit.js
@@ -1,14 +1,35 @@
 var Trait = require('traits.js').Trait;
+var ContextManager = require("./contextManager.js");
+
 
 TGuidedVisitOperations = Trait({
+	addDescription: function(lang, desc) {
+		this.description[lang] = desc;
+	},
+	getDescription: function() {
+		return this.description[this.currentLanguage];
+	},
+	setLanguage: function(lang) {
+		this.currentLanguage = lang;
+	},
 	updateItineraries: function() {
 		
 	},
 	addPoiAnnotations: function() {
-		
+		var pois = this.currentIninerary.itineraryPOIs;
+		for(i=0; i<pois.length; i++) {
+			var poi = pois[i];
+			var pos = [poi.latitude, poi.longitude];
+			this.annotations[pos] = {
+										"poi": poi.name[poi.language]},
+										"state": poi.state
+									};
+		}
 	},
-	setItinerary: function() {
-		
+	setItinerary: function(id) {
+		if(typeof this.itineraryList[id] !== 'undefined') {
+			this.currentItinerary = this.itineraryList[id];
+		}
 	},
 	checkNearestPoit: function() {
 		
@@ -31,8 +52,10 @@ TGuidedVisitOperations = Trait({
 function TGuidedVisit(itList, newAnnotations, currentIti, currentPoi) {
 	return Trait.compose(TGuidedVisitOperations, 
 		Trait({
-			itinerariList: [],
-			annotations: [],
+			itineraryList: [],
+			currentLanguage: "en",
+			description: new Map(),
+			annotations: new Map(),  //map view annotated with all pois {(lat, long), annotation}
 			currentItinerary: currentIti, //int
 			currentPOI: currentPoi
 		}));
