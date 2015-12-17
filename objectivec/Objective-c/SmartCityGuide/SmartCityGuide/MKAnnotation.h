7d1adf8357ba241a0d1a95d0d9b468ee2190fde5
Nicolas Cardozo <ncardozo@vub.ac.be>
2015-12-17 16:45:10 +0000

    added annotation class
    Signed-off-by: Nicolas Cardozo <ncardozo@vub.ac.be>

---
+++ objectivec/Objective-c/SmartCityGuide/SmartCityGuide/MKAnnotation.h
@@ -0,0 +1,33 @@
+//
+//  MKAnnotation.h
+//  DemoGuide
+//
+//  Created by Guillaume Kaisin on 02/02/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <MapKit/MapKit.h>
+#import "SmartCityGuideAppDelegate.h"
+#import "DescViewController.h"
+
+@interface MKAnnotation : NSObject
+<MKAnnotation>{
+	NSString *title;
+	CLLocationCoordinate2D coordinate;
+    CLLocation * location;
+    Poi * poi;
+    int indexIti;
+    
+}
+
+@property (nonatomic, copy) NSString *title;
+@property (nonatomic, readonly) CLLocationCoordinate2D coordinate;
+@property (nonatomic, retain) CLLocation * location;
+@property (nonatomic, retain) Poi * poi;
+@property (nonatomic, assign) int indexIti;
+
+- (id)initWithPoi:(Poi*)p;
+- (id)initWithPoi:(Poi*)p index:(int)i;
+
+@end
---
+++ objectivec/Objective-c/SmartCityGuide/SmartCityGuide/MKAnnotation.m
@@ -0,0 +1,38 @@
+//
+//  MKAnnotation.m
+//  DemoGuide
+//
+//  Created by Guillaume Kaisin on 02/02/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "MKAnnotation.h"
+
+@implementation MKAnnotation
+@synthesize title, coordinate, poi, indexIti, location;
+
+- (id)initWithPoi:(Poi*)p{
+	self = [super init];
+    CLLocationCoordinate2D annotationCoord;
+    annotationCoord.latitude = [p latitude];
+    annotationCoord.longitude = [p longitude];
+	self.title = [p name];
+	coordinate = annotationCoord;
+    self.poi = p;
+    self.location = [[CLLocation alloc] initWithLatitude:coordinate.latitude longitude:coordinate.longitude];
+	return self;
+}
+
+- (id)initWithPoi:(Poi*)p index:(int)i{
+	self = [super init];
+    CLLocationCoordinate2D annotationCoord;
+    annotationCoord.latitude = [p latitude];
+    annotationCoord.longitude = [p longitude];
+	self.title = [p name];
+	coordinate = annotationCoord;
+    self.poi = p;
+    self.indexIti = i;
+	return self;
+}
+
+@end
