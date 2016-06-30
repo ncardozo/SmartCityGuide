b8dd76924c7041480cd27e2b7b7f02399e5a9781
Nicolas Cardozo <ncardozo@vub.ac.be>
2016-06-30 10:32:13 -0500

    added missing management methods to the context manager
    Signed-off-by: Nicolas Cardozo <ncardozo@vub.ac.be>

--- js/js/contextManager.js
+++ js/js/contextManager.js
@@ -6,6 +6,7 @@ var POI = require("./poi.js");
 var ContextManager = function() {
   this.currentLocation = [];
   this.currentDay = new Date();
+  this.currentLanguage = "EN";
 };
 
 ContextManager.prototype.updateLocation = function() {
@@ -14,6 +15,35 @@ ContextManager.prototype.updateLocation = function() {
     extIp = ip;
   });
   this.currentLocation = geoip.lookup(extIp);
+};
+
+ContextManager.prototype.setLanguage = function(lang) {
+  this.currentLanguage = lang;
+  this.updateLanguage();
+};
+
+ContextManager.prototype.updateLanguage = function() {
+  if(this.currentLanguage == "EN") {
+      POI.Poi.setStrategy(new POI.EnglishPoiStrategy());
+  } else if(this.currentLanguage == "FR") {
+    POI.Poi.setStrategy(new POI.FrenchPoiStrategy());
+  } else if(this.currentLanguage == "NL") {
+    POI.Poi.setStrategy(new POI.DutchPoiStrategy());
+  }
+};
+
+ContextManager.prototype.updateAudience = function(audience) {
+  if(audience == "KID") {
+    if(this.currentLanguage == "EN") {
+      POI.Poi.setStrategy(new POI.KidENPoiStrategy());
+    } else if(this.currentLanguage == "FR") {
+      POI.Poi.setStrategy(new POI.KidFRPoiStrategy());
+    } else if(this.currentLanguage == "NL") {
+      POI.Poi.setStrategy(new POI.KidNLPoiStrategy());
+    }
+  } else {
+    POI.Poi.setStrategy(new POI.PoiStrategy());
+  }
 };
 
 ContextManager.prototype.updateDate = function() {
@@ -30,15 +60,19 @@ ContextManager.prototype.updateDate = function() {
 
 ContextManager.prototype.updateConnectivityStatus = function() {
   var type = navigator.connection.type;
-  if(type === "wifi") {
+  if(type === "WIFI") {
 
-  } else if(type === "cellular") {
+  } else if(type === "CELLULAR") {
 
-  } else if(type === "none") {
+  } else if(type === "NONE") {
 
   }
 };
 
 ContextManager.prototype.updateMemory = function() {
-  
+
+};
+
+module.exports = {
+	ContextManager: ContextManager
 };
