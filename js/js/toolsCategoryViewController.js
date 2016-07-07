8939aa69639d5ff36d5a4c5fe3d925e984cb3396
Nicolas Cardozo <ncardozo@vub.ac.be>
2016-07-07 09:49:13 -0500

    added tools view category
    Signed-off-by: Nicolas Cardozo <ncardozo@vub.ac.be>

---
+++ js/js/toolsCategoryViewController.js
@@ -0,0 +1,44 @@
+var CATEGORY = require("./category.js");
+
+var ToolsCategoryViewControllerStrategy = function() {
+  this.categoryList = [];
+};
+
+ToolsCategoryViewControllerStrategy.prototye.tableView = function(view, index) {
+  var category = this.categoryList[index];
+  console.log(category.getName());
+  console.log("Category color Black");
+};
+
+//ADAPTATIONS
+var ColorToolsCategoryViewControlelrStrategy = function() {};
+ColorToolsCategoryViewControlelrStrategy.prototye.tableView = function(view, index) {
+  var category = categoryList[index];
+
+  console.log(category.getName());
+  console.log("Category color " + category.getColor());
+};
+
+//
+var ToolsCategoryViewController = function(strategy) {
+  this.strategy = strategy;
+};
+
+ToolsCategoryViewController.prototype.setStrategy = function(strategy) {
+  this.strategy = strategy;
+};
+
+ToolsCategoryViewController.prototype.getStrategy = function() {
+  return this.strategy;
+};
+
+ToolsCategoryViewController.prototype.tableView = function(view, index) {
+  this.strategy.tableView(view, index);
+};
+
+
+module.exports = {
+  ToolsCategoryViewController: ToolsCategoryViewController,
+  ToolsCategoryViewControllerStrategy: ToolsCategoryViewControllerStrategy,
+
+};
