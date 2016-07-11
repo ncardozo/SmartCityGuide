537fe44c725df8f29ed25991ad524f250d4acc9c
Nicolas Cardozo <ncardozo@vub.ac.be>
2016-07-11 18:37:54 -0500

    added contexts definitions
    Signed-off-by: Nicolas Cardozo <ncardozo@vub.ac.be>

---
+++ Ambience/Ambience/contexts.lisp
@@ -0,0 +1,12 @@
+(in-package #:cl-user)
+(defpackage #:scg (:use #:amos-cl))
+(in-package #:scg)
+
+(defcontext @english)
+(defcontext @french)
+(defcontext @dutch)
+(defcontext @morning)
+(defcontext @afternoon)
+(defcontext @night)
+(defcontext @guided-tour)
+(defcontext @free-visit)
