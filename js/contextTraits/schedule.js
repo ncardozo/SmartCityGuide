b2d77e8994f92b551ab984e0fffb8986874ceb9f
Nicolas Cardozo <ncardozo@vub.ac.be>
2016-07-11 15:42:48 -0500

    added the schedule trait
    Signed-off-by: Nicolas Cardozo <ncardozo@vub.ac.be>

---
+++ js/contextTraits/schedule.js
@@ -0,0 +1,41 @@
+var Trait = require("traits").Trait;
+var cop = require("context-traits");
+var db = require("./DBManager.js");
+
+TScheduleOperations = Trait({
+  setOpeningTimes: function(newMap) {
+  	var self = this;
+  	newMap.forEach(function(key, value) {
+  		self.openingTimes.set(key, value);
+  	});
+  },
+  changeOpeningTime: function(day, start, end) {
+  	var s = this.openingTimes.get(day);
+  	s[opening] = start;
+  	s[closing] = end;
+  	this.openingTimes.set(dat, s);
+  },
+  isInSchedule: function(date) {
+  	var times = this.openingTimes.get(date.getDay());
+  	if(times[opening].getHours() <= date.getHours() && times[closing].getHours() >= date.getHours()) {
+  		return true;
+  	}
+  	return false;
+  }
+});
+
+function TSchedule() {
+  return Trait.compose(TScheduleOperations, Trait({
+    openingTimes: new Map() //day -> object map where object {opening: Date, closing: Date} and days are numbered
+  }));
+}
+
+function Schedule() {
+	return Object.create(Object.prototype, TSchedule());
+}
+
+
+module.exports = {
+	Schedule: Schedule,
+	TScheduleOperations: TScheduleOperations
+};
